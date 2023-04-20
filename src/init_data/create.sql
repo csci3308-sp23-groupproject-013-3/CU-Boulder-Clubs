5315fe595df3011289f510c65b8205c10a416af7
Matthew Bloomfield <mabl4638@colorado.edu>
2023-04-20 09:00:27 -0600

    create.sql update

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,15 +1,13 @@
-<<<<<<< Updated upstream
 CREATE TABLE IF NOT EXISTS clubs(
   club_id SERIAL PRIMARY KEY,   /* the primary key for each entry */
   club_name VARCHAR(120) NOT NULL,
   category VARCHAR(120),
   meeting_time VARCHAR(120),
   location VARCHAR(120),
-  memebrs SMALLINT
-=======
+  members SMALLINT
+);
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(60) NOT NULL
->>>>>>> Stashed changes
 );
