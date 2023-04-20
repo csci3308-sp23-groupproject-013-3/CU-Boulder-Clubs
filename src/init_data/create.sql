7a354621a55d0f4cc404ea285b5ce00bf3874942
Matthew Bloomfield <mabl4638@colorado.edu>
2023-04-20 09:29:12 -0600

    add discover.ejs, work on review sql and ejs

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS reviews (
+    review_id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    rating INTEGER NOT NULL,
+    user_id VARCHAR NOT NULL REFERENCES users(username),
+    club_id INTEGER NOT NULL REFERENCES clubs(club_id),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
 CREATE TABLE IF NOT EXISTS clubs(
   club_id SERIAL PRIMARY KEY,   /* the primary key for each entry */
   club_name VARCHAR(120) NOT NULL,
